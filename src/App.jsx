091be398dcce9c9654d8270ad3f05755ed33c5eb
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-12-02 01:16:49 -0500

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-red-500 flex justify-center">Test</h1>
+    </div>
+  );
+}
+
+export default App;
