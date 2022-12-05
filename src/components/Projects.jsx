ef5a2b99f6f8266b680aec78b89b7d5c1752e37b
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-12-05 16:11:24 -0500

    slight change to project component padding in small screen

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
   ];
   return (
     <div name="Projects" className="md:h-screen w-full">
-      <div className="flex flex-col justify-center w-full h-full max-w-[1100px] mx-auto p-3 pt-16 sm:pt-0">
+      <div className="flex flex-col justify-center w-full h-full max-w-[1100px] mx-auto p-3 pt-16 sm:pt-14 md:pt-0">
         <h1 className="text-3xl pb-3">My Projects</h1>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
           {projectCardData.map(({ id, title, overview }) => (
