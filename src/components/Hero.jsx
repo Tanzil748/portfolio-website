1d0eab92f100f723cf35af06f12b4ed60d5007b1
Tanzil Hassan <tanzilhassan333@gmail.com>
2023-03-09 22:19:56 -0500

    Hero/About section redone

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,62 +2,105 @@ import React from "react";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
 import { motion } from "framer-motion";
+import { heroImg } from "../assets";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
+import { TypeAnimation } from "react-type-animation";
+import "../styles/hero.css";
+import { fadeIn } from "./pageAnimation";
 
 const Hero = () => {
   return (
-    <div
-      name="Home"
-      className="h-screen w-full pt-4 flex justify-center items-center snap-center"
-    >
-      <div>
-        <motion.p
-          className="text-5xl py-2 text-slate-300"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.5, duration: 1.5 }}
+    <section className="h-screen flex justify-center items-center">
+      <div className="xl:container mx-auto grid grid-cols-1 lg:grid-cols-2">
+        <motion.div
+          variants={fadeIn("up")}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: true }}
+          className="flex flex-col justify-center items-center lg:items-start lg:pl-3"
         >
-          Tanzil Hassan
-        </motion.p>
-        <motion.p
-          className="text-xl pb-2 text-slate-400"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.0, duration: 1.5 }}
+          <div className="text-4xl py-2 text-slate-300">Hi, I'm</div>
+          <div className="text-5xl py-2 text-slate-300">TANZIL HASSAN</div>
+          <div>
+            <TypeAnimation
+              sequence={[
+                "Frontend Developer",
+                3000,
+                "Full Stack Developer",
+                3000,
+                "UI/UX Enthusiast",
+                3000,
+              ]}
+              wrapper="div"
+              repeat={Infinity}
+              className="text-red-500 text-4xl"
+            />
+          </div>
+          <motion.ul
+            className="flex gap-3 pt-7"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 2.0, duration: 1.5 }}
+          >
+            <li>
+              <a href="https://github.com/Tanzil748" target="_blank">
+                <AiFillGithub size={30} className="hover:scale-110" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/tanzilhassan/"
+                target="_blank"
+              >
+                <AiFillLinkedin
+                  size={30}
+                  color="#0072b1"
+                  className="hover:scale-110"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="mailto:tanzilhassan333@gmail.com">
+                <MdOutlineMarkEmailUnread
+                  color="orange"
+                  size={30}
+                  className="hover:scale-110"
+                />
+              </a>
+            </li>
+          </motion.ul>
+        </motion.div>
+
+        <motion.div
+          className="hidden lg:block relative h-[450px]"
+          variants={fadeIn("down")}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: true }}
         >
-          Front End & Full Stack Developer
-        </motion.p>
-        <motion.ul
-          className="flex gap-3 justify-center"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5, duration: 1.5 }}
-        >
-          <li>
-            <a href="https://github.com/Tanzil748" target="_blank">
-              <AiFillGithub size={30} className="hover:scale-110" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/tanzilhassan/" target="_blank">
-              <AiFillLinkedin
-                size={30}
-                color="#0072b1"
-                className="hover:scale-110"
+          {/* three js blob animation inspired from lama dev tutorial */}
+          <Canvas>
+            <OrbitControls enableZoom={false} />
+            <ambientLight intensity={0.3} />
+            <directionalLight position={[2, 2, 1]} />
+            <Sphere args={[1, 100, 200]} scale={2.9}>
+              <MeshDistortMaterial
+                color="blue"
+                attach="material"
+                distort={0.3}
+                speed={2}
               />
-            </a>
-          </li>
-          <li>
-            <a href="mailto:tanzilhassan333@gmail.com">
-              <MdOutlineMarkEmailUnread
-                color="orange"
-                size={30}
-                className="hover:scale-110"
-              />
-            </a>
-          </li>
-        </motion.ul>
+            </Sphere>
+          </Canvas>
+          <img
+            src={heroImg}
+            alt="hero image"
+            className="w-96 heroImgMov absolute"
+          />
+        </motion.div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,54 +1,58 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
-import { project1, project2 } from "../assets";
+import { project1, project1Updated, project2, project4 } from "../assets";
 import { motion } from "framer-motion";
 
 const Projects = () => {
   const projectCardData = [
     {
       id: 1,
-      title: "Ecommerce Site",
-      overview: project1,
-      githubLink: "#",
+      title: "Woodside Bazaar",
+      overview: project4,
+      githubLink: "https://github.com/Tanzil748/woodside-ecommerce",
       demo: "#",
     },
     {
       id: 2,
-      title: "Crypto App",
-      overview: project1,
-      githubLink: "#",
-      demo: "#",
-    },
-    {
-      id: 3,
       title: "ANI-MANGO",
       overview: project2,
       githubLink: "https://github.com/Tanzil748/ani-man_go/tree/main",
       demo: "https://tanzil748.github.io/ani-man_go/",
     },
     {
+      id: 3,
+      title: "Uno Royale",
+      overview: project1,
+      githubLink: "https://github.com/Tanzil748/uno-royale",
+      demo: "#",
+    },
+    {
       id: 4,
       title: "QC TROPA Club Website",
-      overview: project1,
+      overview: project1Updated,
       githubLink: "https://github.com/Tanzil748/club-website",
       demo: "https://tanzil748.github.io/club-website/",
     },
   ];
   return (
-    <div name="Projects" className="md:h-screen w-full snap-start">
-      <div className="flex flex-col justify-center w-full h-full mx-auto p-3 pt-16 sm:pt-14 md:pt-0 max-w-6xl">
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ duration: 3, type: "spring", bounce: 0.1 }}
-          variants={{
-            visible: { opacity: 1, translateX: 0 },
-            hidden: { opacity: 0, translateX: 100 },
-          }}
-        >
+    <section
+      name="Projects"
+      className="md:h-screen w-full snap-start scroll-mt-20"
+    >
+      {/* <div className="flex flex-col justify-center w-full h-full mx-auto p-3 pt-16 sm:pt-14 md:pt-0 max-w-6xl"> */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 3, type: "spring", bounce: 0.1 }}
+        variants={{
+          visible: { opacity: 1, translateX: 0 },
+          hidden: { opacity: 0, translateX: 100 },
+        }}
+      >
+        <div className="lg:container mx-auto px-2">
           <h1 className="text-3xl pb-3 text-slate-300">My Projects</h1>
-          <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+          <div className="grid sm:grid-cols-2 gap-2">
             {projectCardData.map(
               ({ id, title, overview, githubLink, demo }) => (
                 <ProjectCard
@@ -61,9 +65,10 @@ const Projects = () => {
               )
             )}
           </div>
-        </motion.div>
-      </div>
-    </div>
+        </div>
+      </motion.div>
+    </section>
+    // </div>
   );
 };
 
