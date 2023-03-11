a3bba69ed158d5ac92b28d024db89cdcf77cb1e2
Tanzil Hassan <tanzilhassan333@gmail.com>
2023-03-11 03:14:52 -0500

    fixed header bug, x button now shows

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
 
   return (
     <motion.header
-      className="bg-[#09203F] text-slate-300 fixed w-full z-[10]"
+      className="bg-[#09203F] text-slate-300 fixed w-full z-10"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 0.5, duration: 1.5 }}
@@ -41,50 +41,47 @@ const Header = () => {
           <div>
             <h1 className="font-bold text-xl">Tanzil Hassan</h1>
           </div>
-          <div>
-            <ul className="hidden sm:flex">
+
+          <ul className="hidden sm:flex">
+            {headerLinks.map(({ name, id }) => (
+              <li
+                key={id}
+                className="px-3 font-bold cursor-pointer hover:scale-110 duration-150"
+              >
+                <Link to={name} smooth duration={500}>
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          <div
+            className="cursor-pointer z-20 sm:hidden"
+            onClick={() => setSideMenu(!sideMenu)}
+          >
+            {sideMenu ? (
+              <AiOutlineClose size={30} />
+            ) : (
+              <RxHamburgerMenu size={30} />
+            )}
+          </div>
+
+          {sideMenu && (
+            <ul className="flex flex-col justify-center items-center w-full h-screen absolute top-0 left-0 bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900">
               {headerLinks.map(({ name, id }) => (
-                <li
-                  key={id}
-                  className="px-3 font-bold cursor-pointer hover:scale-110 duration-150"
-                >
-                  <Link to={name} smooth duration={500}>
+                <li key={id} className="text-2xl cursor-pointer py-4 font-bold">
+                  <Link
+                    onClick={() => setSideMenu(!sideMenu)}
+                    to={name}
+                    smooth
+                    duration={500}
+                  >
                     {name}
                   </Link>
                 </li>
               ))}
             </ul>
-            <div
-              className="cursor-pointer z-20 sm:hidden"
-              onClick={() => setSideMenu(!sideMenu)}
-            >
-              {sideMenu ? (
-                <AiOutlineClose size={30} />
-              ) : (
-                <RxHamburgerMenu size={30} />
-              )}
-            </div>
-
-            {sideMenu && (
-              <ul className="flex flex-col justify-center items-center w-full h-screen absolute top-0 left-0 bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900">
-                {headerLinks.map(({ name, id }) => (
-                  <li
-                    key={id}
-                    className="text-2xl cursor-pointer py-4 font-bold"
-                  >
-                    <Link
-                      onClick={() => setSideMenu(!sideMenu)}
-                      to={name}
-                      smooth
-                      duration={500}
-                    >
-                      {name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
+          )}
         </div>
       </div>
     </motion.header>
