e3a194dea387f2fec6b0d5bdc562e529f411617a
Tanzil Hassan <tanzilhassan333@gmail.com>
2023-03-10 22:40:38 -0500

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import TechStack from "./components/TechStack";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Projects />
       <TechStack />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#09203F] py-2">
+      <div className="xl:container mx-auto">
+        <div className="flex flex-col justify-center items-center text-white">
+          <p>©Tanzil Hassan</p>
+          <p className="text-center">
+            2 5d PNG Designed By 588ku from <br />
+            https://pngtree.com/freepng/2-5d-computer-and-men-s-electronic-products-free-map_4542973.html?sol=downref&id=bef
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
